51e9ed348e12d5a6d21df3399be1f2011264e48e
Felice Ferri <feliceferri@gmail.com>
2020-03-24 23:17:39 -0400

    Restaurant Model

---
+++ Shared/DBModels/Restaurant.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shared.DBModels
+{
+    public class Restaurant: BaseModel
+    {
+        [MaxLength(50)]
+        public string Name { get; set; }
+        [DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        [MaxLength(20)]
+        public string OpeningHours { get; set; }
+        public Decimal Latitude { get; set; }
+        public Decimal Longitude { get; set; }
+    }
+}
