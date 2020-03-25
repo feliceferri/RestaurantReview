a6d7a99d1ffe47874558830ec0a27427e3b34469
Felice Ferri <feliceferri@gmail.com>
2020-03-24 23:07:41 -0400

    Reviews Model

---
+++ Shared/DBModels/Reviews.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Shared.DBModels
+{
+    public class Reviews: BaseModel
+    {
+        public int Rating { get; set; }
+        public DateTime VisitDate { get; set; }
+        
+        [DataType(DataType.MultilineText)]
+        public string Comment { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string ReplyByTheOwner { get; set; }
+    }
+}
