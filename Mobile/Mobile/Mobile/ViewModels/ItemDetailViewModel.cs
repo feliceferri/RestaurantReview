97089bd772405492ec9e14582427fbca5b29c150
Felice Ferri <feliceferri@gmail.com>
2020-03-28 23:08:47 -0400

    Mobile - Xamarin

---
+++ Mobile/Mobile/Mobile/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Mobile.Models;
+
+namespace Mobile.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
